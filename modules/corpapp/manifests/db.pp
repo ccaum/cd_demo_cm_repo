7311c900fb9112a696c0fb4d1afbc311bb487df8
root <root@puppetmaster.vagrant.internal>
2013-08-13 20:24:39 +0000

    Fix dynamic app configuration per environment

--- modules/corpapp/manifests/db.pp
+++ modules/corpapp/manifests/db.pp
@@ -4,14 +4,15 @@ class corpapp::db {
   $db_user     = hiera('wordpress::db_user')
   $db_password = hiera('wordpress::db_password')
   $wordpress_directory = hiera('wordpress::install_dir')
+  $wordpress_server = hiera('wordpress::server')
 
   file { $wordpress_directory:
     ensure => directory,
   }
 
   file { "${wordpress_directory}/import.sql":
-    ensure => file,
-    source => 'puppet:///modules/corpapp/import.sql',
+    ensure  => file,
+    content => template('corpapp/import.sql.erb'),
   }
 
   mysql::db { 'wordpress':
