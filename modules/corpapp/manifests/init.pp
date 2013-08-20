1e146af4eafcb4db224eeb8c69a5c5cc2bd0e23a
Carl Caum <carl@carlcaum.com>
2013-08-20 22:53:28 +0000

    Add explicit file dependencies for wordpress

--- modules/corpapp/manifests/init.pp
+++ modules/corpapp/manifests/init.pp
@@ -11,6 +11,13 @@ class corpapp {
     docroot    => $wordpress::install_dir,
   }
 
+  file { "${wordpress::install_dir}/wp-content":
+    ensure => directory,
+    owner  => 'www-data',
+    mode   => '0640',
+    notify => Service['httpd'],
+  }
+
   file { "${wordpress::install_dir}/wp-content/uploads":
     ensure => directory,
     owner  => 'www-data',
@@ -18,7 +25,7 @@ class corpapp {
     notify => Service['httpd'],
   }
 
-  file { '/opt/wordpress/wp-content/uploads/2013': 
+  file { "${wordpress::install_dir}/wp-content/uploads/2013:
     ensure  => directory,
     source  => 'puppet:///modules/corpapp/2013',
     owner   => 'www-data',
@@ -26,7 +33,7 @@ class corpapp {
     notify  => Service['httpd'],
   }
 
-  file { '/opt/wordpress/wp-content/uploads/2013/08': 
+  file { "${wordpress::install_dir}/wp-content/uploads/2013/08:
     ensure  => directory,
     source  => 'puppet:///modules/corpapp/2013/08',
     owner   => 'www-data',
@@ -34,7 +41,7 @@ class corpapp {
     notify  => Service['httpd'],
   }
 
-  file { '/opt/wordpress/wp-content/uploads/2013/08/banner-shirtback.jpg': 
+  file { "/opt/wordpress/wp-content/uploads/2013/08/banner-shirtback.jpg": 
     ensure  => file,
     source  => 'puppet:///modules/corpapp/2013/08/banner-shirtback.jpg',
     owner   => 'www-data',
